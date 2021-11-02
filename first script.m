4ccb87a469abc1f614cf1e592975e66e487fc5a8
Reemo55 <93342993+Reemo55@users.noreply.github.com>
2021-11-02 22:50:24 +0200

    Add files via upload

---
+++ first script.m
@@ -0,0 +1,45 @@
+a=[-7 5 -9;2 -1 2;1 -1 2 ];
+b=[16 3 2 13;5 10 11 8; 9 6 7 12;4 15 14 1];
+c=[4 2 -3;7 -7 9; 3 -5 6];
+d=[6 3 2;2 12 -7 ;-1 6 2;-5 15 11];
+%% question 1
+a=[-7 5 -9;2 -1 2;1 -1 2 ];
+b=[16 3 2 13;5 10 11 8; 9 6 7 12;4 15 14 1];5 6];
+c=[4 2 -3;7 -7 9; 3 -5 6];
+d=[6 3 2;2 12 -7 ;-1 6 2;-5 15 11];
+
+3*a-5*c
+% 7*a+2*b %that is wrong because the dimentions aren't equal
+x=c*d'
+s=c*a
+%% question 2
+d=[6 3 2;2 12 -7 ;-1 6 2;-5 15 11];
+n=3
+m=4
+r=zeros(n)
+l=zeros(m,n)
+f=ones(n)
+w=ones(m,n)
+t=size(d)
+h=zeros(4,3)
+u=diag([1 2 3 4])
+%% qustion 3
+a=[-7 5 -9;2 -1 2;1 -1 2 ];
+b=[16 3 2 13;5 10 11 8; 9 6 7 12;4 15 14 1];%
+[a,b]
+%[a;b] eror because the dimentions of matrices arn't consistent
+%% question 4
+s=zeros(7,8)
+s=diag([5 5 5 5 5 5 5])
+s(:,8)=[5;5;5;5;5;5;5]
+%% question 5
+a=[-7 5 -9;2 -1 2;1 -1 2 ];
+i=2
+j=3
+p=a(i,:)
+y=a(:,j)
+
+
+
+
+
